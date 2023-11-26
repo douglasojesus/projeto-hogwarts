cc03a0d827087c111c076d454eec5af55681360b
Douglas Jesus <dougecomp@gmail.com>
2023-11-26 13:38:34 -0300

    removing settings

--- hogwarts/hogwarts/settings.py
+++ hogwarts/hogwarts/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -65,9 +64,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
-ALLOWED_HOSTS = ['your-app-name.herokuapp.com']
-
 
 ROOT_URLCONF = 'hogwarts.urls'
 
@@ -141,6 +137,3 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
