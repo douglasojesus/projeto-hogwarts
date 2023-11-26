f6a7c46858f75169c2f8bfadd67b9b7be9419561
Douglas Jesus <dougecomp@gmail.com>
2023-11-26 11:42:01 -0300

    testing deploy

--- hogwarts/hogwarts/settings.py
+++ hogwarts/hogwarts/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
+django_heroku.settings(locals())
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
